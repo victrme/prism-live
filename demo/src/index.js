dfb3ca28e422c5e8394f0571951547250211dd7d
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2024-03-03 19:17:35 +0100

    Added languages to demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,72 +1,70 @@
-import PrismLive from "../../src/prism-live.js";
-import "../../src/prism-live.css";
+import PrismLive from "@victr/prism-live";
+import javascript from "@victr/prism-live/javascript";
+import markup from "@victr/prism-live/markup";
+import css from "@victr/prism-live/css";
+import "@victr/prism-live/style.css";
 
-const html = `<html lang="en">
-<head>
-	<meta charset="UTF-8" />
-	<title>Prism Live: Lightweight, extensible, powerful web-based code editor</title>
-	<link rel="stylesheet" href="./src/style.css" />
-</head>
-<body>
-	<header>
-		<h1><img src="https://prismjs.com/assets/logo.svg" alt="Prism" /> Live!</h1>
-		<h2>Lightweight, extensible, powerful web-based code editor</h2>
-	</header>
-</body>
-</html>`;
-
-const css = `div.prism-live {
-	position: relative;
-	box-sizing: border-box;
-	display: flex;
-	flex-flow: column;
-}
-
-textarea.prism-live,
-pre.prism-live {
-	padding: 0.2rem 0.5rem;
-	box-sizing: border-box;
-	margin: 0;
-}
-
-@supports (not (caret-color: black)) and (-webkit-text-fill-color: black) {
-	textarea.prism-live {
-		color: inherit;
-		-webkit-text-fill-color: transparent;
-	}
-}`;
-
-const js = `var ret = {};
-var canonical = new WeakMap(
-	Object.entries(Prism.languages)
-		.map((x) => x.reverse())
-		.reverse()
-);
-
-for (var id in Prism.languages) {
-	var grammar = Prism.languages[id];
-
-	if (typeof grammar !== "function") {
-		ret[id] = canonical.get(grammar);
-	}
-}
-
-return ret;`;
-
-//
-
+PrismLive.registerLanguage(javascript);
+PrismLive.registerLanguage(markup);
+PrismLive.registerLanguage(css);
 PrismLive.createAll();
 
 document.querySelectorAll("textarea.language-html.fill").forEach((textarea) => {
-	textarea.value = html;
+	textarea.value = `<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Prism Live: Lightweight, extensible, powerful web-based code editor</title>
+		<link rel="stylesheet" href="./src/style.css" />
+	</head>
+	<body>
+		<header>
+			<h1><img src="https://prismjs.com/assets/logo.svg" alt="Prism" /> Live!</h1>
+			<h2>Lightweight, extensible, powerful web-based code editor</h2>
+		</header>
+	</body>
+	</html>`;
 });
 
 document.querySelectorAll("textarea.language-css.fill").forEach((t) => {
-	t.value = css;
+	t.value = `div.prism-live {
+		position: relative;
+		box-sizing: border-box;
+		display: flex;
+		flex-flow: column;
+	}
+	
+	textarea.prism-live,
+	pre.prism-live {
+		padding: 0.2rem 0.5rem;
+		box-sizing: border-box;
+		margin: 0;
+	}
+	
+	@supports (not (caret-color: black)) and (-webkit-text-fill-color: black) {
+		textarea.prism-live {
+			color: inherit;
+			-webkit-text-fill-color: transparent;
+		}
+	}`;
 	t.dispatchEvent(new InputEvent("input"));
 });
 
 document.querySelectorAll("textarea.language-js.fill").forEach((t) => {
-	t.value = js;
+	t.value = `var ret = {};
+	var canonical = new WeakMap(
+		Object.entries(Prism.languages)
+			.map((x) => x.reverse())
+			.reverse()
+	);
+	
+	for (var id in Prism.languages) {
+		var grammar = Prism.languages[id];
+	
+		if (typeof grammar !== "function") {
+			ret[id] = canonical.get(grammar);
+		}
+	}
+	
+	return ret;`;
 	t.dispatchEvent(new InputEvent("input"));
 });
